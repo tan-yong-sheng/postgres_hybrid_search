2672da4fff582e2a50f67a2943340a6172f4de87
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-10-12 04:28:03 +0000

    update

--- tests/project/utils/test_nlp_handler.py
+++ tests/project/utils/test_nlp_handler.py
@@ -42,6 +42,11 @@ expected_match1 = [
     },
     {
         "exchange": "Bursa",
+        "entity_name": "OPCOM HOLDINGS",
+        "entity_type": "COMPANY_NAME",
+    },
+    {
+        "exchange": "Bursa",
         "entity_name": "ADVANCECON HOLDINGS",
         "entity_type": "COMPANY_NAME",
     },
@@ -63,4 +68,5 @@ def test_unit_extract_financial_entities(text, expected_match):
     from project.utils.nlp_handler import extract_financial_entities
 
     matches = extract_financial_entities(text)
+    print(matches)
     assert matches == expected_match
