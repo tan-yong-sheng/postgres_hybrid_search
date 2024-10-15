d4af958644a4309418cd0f140dd45c5c250460ce
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-10-15 17:32:51 +0000

    update

--- project/tasks/full_text_search_news.py
+++ project/tasks/full_text_search_news.py
@@ -1,25 +1,42 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from project.db_models import NewsOrm
+from project.db_models import NewsOrm, NewsToStockSymbol, StockSymbolOrm
 from project.schemas.news_schema import NewsSearchReturn
 
 
 # note: need to add pydantic model for the response
-def find_news_with_keywords(db: Session, query, limit=10):
+def find_news_with_keywords(
+    db: Session, query: str, companies_name: list[str], limit: int = 10
+):
+    stock_ids = [
+        stock_id[0]
+        for stock_id in (
+            db.query(StockSymbolOrm.id)
+            .filter(StockSymbolOrm.company_name.in_(companies_name))
+            .all()
+        )
+    ]
+    news_ids = [
+        news_id[0]
+        for news_id in (
+            db.query(NewsToStockSymbol.news_id)
+            .filter(NewsToStockSymbol.stock_symbol_id.in_(stock_ids))
+            .all()
+        )
+    ]
+
     results = (
         db.query(
             NewsOrm.title,
             NewsOrm.content,
-            func.ts_rank(
-                NewsOrm.fts, func.plainto_tsquery("english", query)
-            ).label("rank"),
+            func.ts_rank(NewsOrm.fts, func.plainto_tsquery("english", query)).label(
+                "rank"
+            ),
         )
-        .filter(NewsOrm.fts.match(query))
+        .filter(NewsOrm.fts.match(query) & NewsOrm.id.in_(news_ids))
         .order_by(
-            func.ts_rank(
-                NewsOrm.fts, func.plainto_tsquery("english", query)
-            ).desc()
+            func.ts_rank(NewsOrm.fts, func.plainto_tsquery("english", query)).desc()
         )
         .limit(limit)
         .all()
@@ -42,6 +59,7 @@ if __name__ == "__main__":
 
     with db_context() as db:
         # Keyword search
-        query = "malaysia"
-        keyword_search_results = find_news_with_keywords(db, query)
+        query = "project"
+        companies_name = ["GENTING BHD", "GENTING MALAYSIA BERHAD"]
+        keyword_search_results = find_news_with_keywords(db, query, companies_name)
         print("Keyword search result: ", keyword_search_results)
--- project/tasks/vector_search_news.py
+++ project/tasks/vector_search_news.py
@@ -1,11 +1,31 @@
+from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import desc
 
-from project.db_models import NewsOrm
+from project.db_models import NewsOrm, NewsToStockSymbol, StockSymbolOrm
 from project.schemas.news_schema import NewsSearchReturn
 
 
-def find_news_with_similar_embeddings(query_embedding, limit=5):
+def find_news_with_similar_embeddings(
+    db: Session, query_embedding: list[float], companies_name: list[str], limit: int = 5
+):
     similarity_threshold = 0.7
+    stock_ids = [
+        stock_id[0]
+        for stock_id in (
+            db.query(StockSymbolOrm.id)
+            .filter(StockSymbolOrm.company_name.in_(companies_name))
+            .all()
+        )
+    ]
+    news_ids = [
+        news_id[0]
+        for news_id in (
+            db.query(NewsToStockSymbol.news_id)
+            .filter(NewsToStockSymbol.stock_symbol_id.in_(stock_ids))
+            .all()
+        )
+    ]
+
     results = (
         db.query(
             NewsOrm.title,
@@ -15,6 +35,7 @@ def find_news_with_similar_embeddings(query_embedding, limit=5):
         .filter(
             NewsOrm.embedding.cosine_distance(query_embedding) < similarity_threshold
         )
+        .filter(NewsOrm.id.in_(news_ids))
         .order_by(desc("score"))
         .limit(limit)
         .all()
@@ -38,7 +59,11 @@ if __name__ == "__main__":
         # Embedding search
         from project.utils.embedding_handler import get_embedding
 
-        query = "Genting malaysia's project"
+        query = "future projection"
+        companies = ["GENTING BHD", "GENTING MALAYSIA BERHAD"]
         query_embedding = get_embedding(input=query)["data"][0]["embedding"]
-        semantic_search_results = find_news_with_similar_embeddings(query_embedding)
+        semantic_search_results = find_news_with_similar_embeddings(
+            db, query_embedding, companies_name=companies
+        )
+        print("Semantic search result: ", semantic_search_results)
         print("Semantic search result: ", semantic_search_results)
