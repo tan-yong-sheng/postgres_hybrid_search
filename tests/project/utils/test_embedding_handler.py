3a2846d7b2fd1aa87d8fe972064524b41e504549
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-10-20 07:06:18 +0000

    update pytest

--- tests/project/utils/test_embedding_handler.py
+++ tests/project/utils/test_embedding_handler.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 import json
+from pathlib import Path
+
 from project.utils.embedding_handler import get_embedding
-
-
 
 
 def mock_output(return_value=None):
@@ -10,18 +9,12 @@ def mock_output(return_value=None):
 
 
 def test_integrate_get_embedding():
-
-
     input = "Hello, world!"
     expected_output = json.load(
         open(Path(__file__).parent / "embedding_output.json", "r")
     )
 
     result = get_embedding(input)
-    print(result)
-
-    print("===================1")
-
     # Assert the result matches the expected output
     assert result == expected_output
     assert len(result["data"][0]["embedding"]) == 384
